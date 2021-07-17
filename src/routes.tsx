86160920c2aa99c122a52f220f8b36d181516ba5
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-17 11:03:28 -0300

    feat: adding pages in the routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,20 @@
 import { Switch, Route } from 'react-router-dom';
 
+import { Dashboard } from './pages/Dashboard';
+import { Login } from './pages/Login';
+import { NewBet } from './pages/NewBet';
+import { Register } from './pages/Register';
+import { ResetPassword } from './pages/ResetPassword';
+
 export function Routes() {
   return (
     <>
       <Switch>
-        <Route path="/" exact />
-        <Route path="/login" />
-        <Route path="/register" />
-        <Route path="/reset_password" />
-        <Route path="/profile" />
-        <Route path="/new_bet" />
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/reset_password" component={ResetPassword} />
+        <Route path="/new_bet" component={NewBet} />
       </Switch>
     </>
   )
