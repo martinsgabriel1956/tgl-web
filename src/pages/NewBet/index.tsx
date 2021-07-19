dc3fb69438c8aecb36b3633889776ef50727b024
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-19 11:12:15 -0300

    feat: complete, clear and add to cart button finished

--- src/pages/NewBet/index.tsx
+++ src/pages/NewBet/index.tsx
@@ -1,9 +1,51 @@
-import { Container } from './styles';
+import { Footer } from "../../components/UI/Footer";
+import { Header } from "../../components/UI/Header";
+
+import {
+  Container,
+  Games,
+  ButtonContainer,
+  ActionButton,
+  AddToCartButton,
+  CartImg,
+} from "./styles";
 
 export function NewBet() {
   return (
-    <Container>
-      <h1>NewBet</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <section>
+          <h2>
+            New Bet <span>For</span>
+          </h2>
+
+          <p>Choose a game</p>
+
+          <Games></Games>
+          <p>Fill Your Bet</p>
+          <span></span>
+
+          <ButtonContainer>
+            <div>
+              <ActionButton>Complete game</ActionButton>
+
+              <ActionButton>Clear game</ActionButton>
+            </div>
+
+            <div>
+              <AddToCartButton>
+                <CartImg />
+                Add to cart
+              </AddToCartButton>
+            </div>
+          </ButtonContainer>
+        </section>
+        <section>
+          <div>dfjkhvfdjfcdks</div>
+        </section>
+      </Container>
+      <Footer />
+    </>
   );
-};
+}
--- src/pages/NewBet/styles.ts
+++ src/pages/NewBet/styles.ts
@@ -1,5 +1,85 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import { FiShoppingCart } from 'react-icons/fi';
 
 export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 
+  section:nth-child(1) {
+    padding: 4rem 7rem;
+  }
+
+  h2 {
+    font-size: 24px;
+    text-transform: uppercase;
+    color: #707070;
+
+    span {
+      font-weight: 200;
+    }
+  }
+  p {
+    color: #868686;
+    font-weight: bold;
+    margin-top: 2rem;
+    font-size: 17px;
+  }
+`;
+
+export const Games = styled.div``;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 2rem;
+
+  div:nth-child(1) button:nth-child(1) {
+    margin-right: 1.5rem;
+  }
+`;
+
+export const ActionButton = styled.button`
+  padding: 0.75rem 1.25rem;
+  border: 2px solid #27c383;
+  color: #27c383;
+  background-color: transparent;
+  border-radius: 0.45rem;
+  font-weight: bold;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #27c383;
+    color: #ffffff;
+    transition: all 0.5s;
+  }
+  `;
+
+export const AddToCartButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  padding: 0.75rem 1.25rem;
+  background-color: #27c383;
+  border: 2px solid #27c383;
+  color: #ffffff;
+  border-radius: 0.45rem;
+  font-weight: bold;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: transparent;
+    color: #27c383;
+    transition: all 0.5s;
+  }
+`;
+
+export const CartImg = styled(FiShoppingCart)`
+  width: 20px;
+  height: 20px;
+
+  margin-right: 1rem;
 `;
